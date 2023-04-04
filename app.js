43e9f15bfa11f7f13e0eaaa60bcc69aa8ed23a5f
Siravit Tanratvijit <siravittanratvijit@Siravits-MacBook-Air.local>
2023-04-04 19:38:40 +0700

    finish

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
+const { error } = require("console");
 const fs = require("fs");
-// console.log(fs);
-
-// const data = fs.readFile("crypto_tax.txt", "utf8", (err, data) => {
-//   return data;
-// });
 
 const taxCal = async () => {
   try {
-    const data = await fs.readFileSync("crypto_tax.txt", { encoding: "utf8" }).split(" \n");
-    const transaction = data.map(el => {
-        const [action, name, price, quantity] = el.split(" ")
-        return {action, name, price: Number(price), qunatity: Number(quantity)}
-    })
+    const data = await fs
+      .readFileSync("crypto_tax.txt", { encoding: "utf8" })
+      .split(" \n");
 
-    console.log(transaction);
-    
+    const transactions = data.map((el) => {
+      const [action, name, price, quantity] = el.split(" ");
+      return { action, name, price: Number(price), quantity: Number(quantity) };
+    });
 
+    // console.log(transactions);
+
+    const wallet = {};
+    let profits = 0;
+
+    for (const transaction of transactions) {
+      const { action, name, price, quantity } = transaction;
+      if (!wallet[name]) {
+        wallet[name] = [];
+      }
+      if (action === "B") {
+        wallet[name].push({ price, quantity });
+      } else if (action === "S") {
+        let soldQuantity = quantity;
+        while (soldQuantity > 0 && wallet[name].length > 0) {
+          const { price: buyPrice, quantity: buyQuantity } = wallet[name][0];
+          
+          const sellQuantity = Math.min(soldQuantity, buyQuantity);
+
+          if (
+            sellQuantity >
+            wallet[name].reduce(
+              (total, transaction) => total + transaction.quantity,
+              0
+            )
+          ) {
+            throw new Error(
+              `Error: cannot sell more ${name} than currently held`
+            );
+          }
+
+          const profit = (price - buyPrice) * sellQuantity;
+          profits += profit;
+          soldQuantity -= sellQuantity;
+          wallet[name][0].quantity -= sellQuantity;
+          if (wallet[name][0].quantity === 0) {
+            wallet[name].shift();
+          }
+        }
+        if (soldQuantity > 0) {
+          throw new Error(
+            `Error: cannot sell more ${name} than currently held`
+          );
+        }
+      }
+    }
+
+    console.log(profits.toFixed(2));
   } catch (err) {
     console.log(err);
   }
